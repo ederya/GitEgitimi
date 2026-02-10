6d6b8a8f5860d0821c3b2e4ab186aa88fc3cd8e7
ederya <emirhanderyaa61@gmail.com>
2026-02-10 21:14:45 +0300

    learn how to use argc and argv parameters.

--- learning/functions.c
+++ learning/functions.c
@@ -3,6 +3,12 @@
 
 int main(int argc, char* argv[]){
 
+    /*
+    argc (Argument Count): Terminale kaç tane kelime yazdığını sayar. 
+    Programın adı dahil olduğu için her zaman en az 1 olur.
+    argv (Argument Vector): Yazdığın kelimelerin listesidir. 
+    argv[0] her zaman ./program_adi dır. Senin yazdığın ilk veri argv[1] de durur.*/
+
     // Eğer sadece programın adını yazdıysan (argüman yoksa)
     if (argc < 2) {
         printf("Lutfen isminizi de yazin! Ornek: ./arg_test Derya\n");
